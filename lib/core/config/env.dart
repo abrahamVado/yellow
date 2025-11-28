d95dd451003ccc7302a4b9dca4cb870a5ce442e3
abrahamVado <abraham.vado@gmail.com>
2025-11-28 10:12:25 -0600

    Update env config

--- lib/core/config/env.dart
+++ lib/core/config/env.dart
@@ -10,7 +10,7 @@ class Env {
   static Future<Env> load() async {
     const baseUrl = String.fromEnvironment(
       'BASE_URL',
-      defaultValue: 'https://api.example.com',
+      defaultValue: 'https://api.softwaremia.com',
     );
     const appEnv = String.fromEnvironment(
       'APP_ENV',
