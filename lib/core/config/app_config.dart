8f6fa966025bbcf5fac1333b02ce001c35bc47f6
abrahamVado <abraham.vado@gmail.com>
2025-12-04 18:32:24 -0600

    chore

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,18 +4,18 @@ class AppConfig {
   static const String appId = 'com.matheydriver.yellow';
   
   // Colors
-  static const Color primaryColor = Color(0xFF000000);
+  static const Color primaryColor = Color(0xFFfafafa);
   static const Color fontColor = Color(0xFFFFFFFF);
-  static const Color waveColor1 = Color(0xFF000000);
-  static const Color waveColor2 = Color(0xFF000000);
-  static const Color buttonColor = Color(0xFFFFDD00);
-  static const Color buttonTextColor = Color(0xFF000000);
+  static const Color waveColor1 = Color(0xFFfafafa);
+  static const Color waveColor2 = Color(0xFFe3c43e);
+  static const Color buttonColor = Color(0xFFf4af0c);
+  static const Color buttonTextColor = Color(0xFFFFFFFF);
 
   // Text
-  static const String title = 'Bienvenido Ivan';
-  static const String subtitle = 'Experimenta una nueva forma de transporte';
+  static const String title = 'Bienvenido';
+  static const String subtitle = 'Experimenta una nueva manera de transporte';
   static const String buttonText = 'Siguiente';
 
   // Assets
-  static const String logoUrl = 'assets/images/logo.jpeg';
+  static const String logoUrl = 'assets/images/logo_padded.jpeg';
 }
